7bc29e16b5240fe447bc6a8851f92330b34b7021
Meco Man <920369182@qq.com>
2021-08-21 11:56:12 -0400

    改由vt100软件包支持

--- cmatrix.c
+++ cmatrix.c
@@ -11,11 +11,9 @@
 
 #include <rtthread.h>
 #include <stdlib.h>
-#include "mcurses.h"
+#include <vt100.h>
 
-#define setLocation(x,y) move(y,x)
-
-static int t[80];
+static int t[VT_DEFAULT_COL_SIZE];
 
 static void sub_d(int p, int s, int x, int y)
 {
@@ -24,115 +22,67 @@ static void sub_d(int p, int s, int x, int y)
 
     r = (p % 16) * 16;
     g = 180 - p;
-    if (r < 10) attrset (F_BLACK);  // forground color black
+    if (r < 10)
+    {
+        vt_set_font_color(VT_F_BLACK);
+    }
     else
     {
-        if (g > 170) attrset(F_WHITE);
-        else if (g > 169) attrset(F_GREEN);
+        if (g > 170)
+        {
+            vt_set_font_color(VT_F_WHITE);
+        }
+        else if (g < 170)
+        {
+            vt_set_font_color(VT_F_GREEN);
+        }
     }
 
-    if ((y >= 0) && (y < 24) && (x < 80))
+    if ((y >= 0) && (y < VT_DEFAULT_ROW_SIZE) && (x < VT_DEFAULT_COL_SIZE))
     {
-        int xx, yy;
-        xx = x;
-        yy = y;
-        setLocation(xx, yy);
         char c;
-        c = 33 + (x * y) % 200;
-        addch(c);
+        c = 33 + (x * y) % 94;
+        vt_draw_char_at(y, x, c);
     }
 }
 
-static int cmatrix(int argc, char const *argv[])
+static void cmatrix(int argc, char const *argv[])
 {
     int i;
     int x;
     int y;
     int k;
 
-    setFunction_putchar(rtt_putchar);   // tell the library which output channel shall be used
+    vt_hide_cursor();
+    vt_store_screen();
+    vt_clearall();
 
-    initscr();      // initialize mcurses
-    curs_set (0);   // set cursor invisible
-    clear();
-
-    for (i = 0; i < 80; i++)
+    for (i = 0; i < VT_DEFAULT_COL_SIZE; i++)
     {
-        t[i] = - random() % 50;
+        //t[i] = - random() % 50;
+        t[i] = - rand() % 50;
     }
 
     //sub_d(1,1,10,10);
     while (1)
     {
-        for (k = 1; k < 80; k++)
+        for (k = 1; k < VT_DEFAULT_COL_SIZE; k++)
         {
-            i = rand() % 79;
+            i = rand() % (VT_DEFAULT_COL_SIZE-1);
             if (t[i] > 28)t[i] = 0;
             t[i] = t[i] + 1;
             y = t[i];
             sub_d( 0     , 0, i, y - 6);
-            sub_d( 2  + x, 0, i, y - 5  );
-            sub_d( 2  + x, 0, i, y - 4  );
-            sub_d( 10 + x, 0, i, y - 3 );
-            sub_d( 10 + x, 0, i, y - 2 );
-            sub_d( 11 + x, 0, i, y - 1 );
-            sub_d( 0     , 2 + x, i, y );
+            sub_d( 2  + x, 0, i, y - 5);
+            sub_d( 2  + x, 0, i, y - 4);
+            sub_d( 10 + x, 0, i, y - 3);
+            sub_d( 10 + x, 0, i, y - 2);
+            sub_d( 11 + x, 0, i, y - 1);
+            sub_d( 0     , 2 + x, i, y);
         }
     }
 
-    return 0;
+    vt_restore_screen();
+    vt_show_cursor();
 }
 MSH_CMD_EXPORT(cmatrix, matrix rain)
-
-
-/*
-QBASIC orginal code from:
-http://codegolf.stackexchange.com/questions/17285/make-the-matrix-digital-rain-using-the-shortest-amount-of-code
-
-DECLARE SUB d (p!, s!, x!, y!)
-DIM t(80)
-FOR i = 1 TO 80
-  t(i) = INT(-50 * RND)
-NEXT
-s = TIMER
-f = 0
-w$ = "bullet"
-o = 1
- * o = 1
-CLS
-WHILE 1
-    FOR i = 1 TO 80
-        IF t(i) > 28 THEN t(i) = 0: IF f THEN SOUND 100 * i, 1
-        t(i) = t(i) + 1
-        y = t(i)
-        d 0, 0, i, y - 6
-        d 2 + x, 0, i, y - 5
-        d 2 + x, 0, i, y - 4
-        d 10 + x, 0, i, y - 3
-        d 10 + x, 0, i, y - 2
-        d 11 + x, 0, i, y - 1
-        d 0, 2 + x, i, y
-    NEXT
-    k$ = INKEY$
-    IF k$ <> "" THEN
-        IF MID$(w$, o, 1) = k$ THEN
-            o = o + 1
-            IF o = LEN(w$) + 1 THEN z = 1: f = 100
-        ELSE
-            o = 1
-        END IF
-
-    END IF
-    x = x + z
-    l = TIMER
-    WHILE l = TIMER
-    WEND
-
-WEND
-
-SUB d (p, s, x, y)
-COLOR p MOD 16, s MOD 16
-IF y > 0 AND y < 24 THEN LOCATE y, x: PRINT CHR$(33 + (x * y) MOD 200);
-END SUB
-*/
- 
